09bdeafa9065f7df226ef2bf5956f244d6c3d4cb
kesen <superkesen@gmail.com>
2017-04-25 12:59:59 +0800

    增加水波纹动画

--- SwiftCollection/Classes/MainViewControllers/水波纹/WaterRipplesDemoVC.swift
+++ SwiftCollection/Classes/MainViewControllers/水波纹/WaterRipplesDemoVC.swift
@@ -12,28 +12,12 @@ class WaterRipplesDemoVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        let path = CGMutablePath()
-        path.move(to: CGPoint(x: 0, y: ScreenHeight * 0.5))
         
-        var y = CGFloat(0)
-        for i in stride(from: 0, to: ScreenWidth+1, by: 1) {
-            
-            // y=Asin（ωx+φ）+ h
-            // A 振幅，决定最大 y 的值
-            // ω决定周期， 周期 = 2π/ω
-            // h: 波形在 y 轴上的偏移
-            
-            y = 100 * sin(CGFloat.pi * 2 / ScreenWidth * i) + 400
-            print(y)
-            path.addLine(to: CGPoint(x: i, y: y))
-        }
-        
-        let lay = CAShapeLayer()
-        lay.fillColor = UIColor.clear.cgColor
-        lay.strokeColor = UIColor.black.cgColor
-        lay.path = path
-        
-        self.view.layer.addSublayer(lay)
+        self .currentWave()
+    }
+    
+    func currentWave() {
+        let waterView = WaterRipplesView(frame: CGRect(x: 0, y: 100, width: ScreenWidth, height: 100))
+        view.addSubview(waterView)
     }
 }
--- SwiftCollection/Classes/MainViewControllers/水波纹/WaterRipplesView.swift
+++ SwiftCollection/Classes/MainViewControllers/水波纹/WaterRipplesView.swift
@@ -9,5 +9,66 @@
 import UIKit
 
 class WaterRipplesView: UIView {
+    
+    lazy var fast: CAShapeLayer = {
+        let layer = CAShapeLayer()
+        layer.strokeColor = UIColor(rgb: 0x1565c0).withAlphaComponent(0.6).cgColor
+        layer.fillColor = UIColor(rgb: 0x1565c0).withAlphaComponent(0.6).cgColor
+        
+        return layer
+    }()
+    
+    lazy var slow: CAShapeLayer = {
+        let layer = CAShapeLayer()
+        layer.strokeColor = UIColor(rgb: 0x1565c0).withAlphaComponent(0.4).cgColor
+        layer.fillColor = UIColor(rgb: 0x1565c0).withAlphaComponent(0.4).cgColor
+        
+        return layer
+    }()
+    
+    lazy var timer: CADisplayLink = CADisplayLink(target: self, selector: #selector(self.timerEvent))
+    
+    let speed = CGFloat(2)
+    
+    // y=Asin（ωx+φ）+ h
+    lazy var A: CGFloat = 20 // 振幅，决定最大 y 的值
+    lazy var ω: CGFloat = CGFloat.pi / self.frame.width // 最小正周期 = 2π/ω，决定一个周期的x轴跨度，此处为一个周期跨两个view的宽度
+    lazy var φ: CGFloat = CGFloat(0)  // 决定在 x 轴上的偏移
+    lazy var h: CGFloat = self.frame.height * 0.5  // 波形在 y 轴上的决定偏移
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        layer.addSublayer(fast)
+        layer.addSublayer(slow)
+        timer.add(to: RunLoop.main, forMode: .commonModes)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func createWave(layer: CAShapeLayer, A: CGFloat, ω: CGFloat, φ: CGFloat, h: CGFloat) {
+        let path: CGMutablePath = CGMutablePath()
+        path.move(to: CGPoint(x: CGFloat(0), y: CGFloat(frame.height / 2)))
+        var y: CGFloat = 0.0
+        
+        for x in stride(from: 0, to: frame.width + 1, by: 1) {
+            y = A * sin(ω * x + φ) + h
+            path.addLine(to: CGPoint(x: CGFloat(x), y: y))
+        }
+        path.addLine(to: CGPoint(x: frame.width, y: frame.height))
+        path.addLine(to: CGPoint(x: CGFloat(0), y: frame.height))
+        path.closeSubpath()
 
+        layer.path = path
+    }
+    
+    @objc private func timerEvent() {
+        // 波纹移动速度
+        φ -= self.frame.width / 60 / 60 / 2
+        
+        createWave(layer: fast, A: self.A, ω: self.ω, φ: self.φ, h: self.h)
+        createWave(layer: slow, A: self.A * 0.5, ω: self.ω * 1.1, φ: self.φ * 0.7 + 100, h: self.h)
+    }
 }
